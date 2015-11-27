5f5f2473749ddd790ea39ce58943a8a527d18678
Jonathan Walker <jwalke24@my.westga.edu>
2015-11-27 14:50:08 -0500

    Apparently, the SerialReader class didn't want to commit.

---
+++ EsploraPulse/Model/SerialReader.cs
@@ -0,0 +1,26 @@
+﻿using System.IO.Ports;
+
+namespace EsploraPulse.Model
+{
+    /// <summary>
+    /// This class is responsible for reading data from
+    /// the Arduino Esplora via a SerialPort object.
+    /// </summary>
+    /// <author>Jonathan Walker</author>
+    /// <version>11/27/2015</version>
+    class SerialReader
+    {
+        private const string PortName = "COM4";
+        private const int BaudRate = 115200;
+
+        private SerialPort port;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SerialReader"/> class.
+        /// </summary>
+        public SerialReader()
+        {
+            this.port = new SerialPort(PortName, BaudRate);
+        }
+    }
+}
