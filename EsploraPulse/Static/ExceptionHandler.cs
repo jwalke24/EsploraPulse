5c5c42705f2cc2fb6926f934f75ac588bd6bbbba
Jonathan Walker <jwalke24@my.westga.edu>
2015-11-27 15:04:49 -0500

    I added an exception handling class to display any exceptions in a user-friendly manner.

---
+++ EsploraPulse/Static/ExceptionHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EsploraPulse.Static
+{
+    /// <summary>
+    /// This class is responsible for presenting exceptions to the user
+    /// in a "friendly" manner.
+    /// </summary>
+    /// <author>Jonathan Walker</author>
+    /// <version>11/27/2015</version>
+    static class ExceptionHandler
+    {
+        /// <summary>
+        /// Displays the error message in a MessageBox.
+        /// </summary>
+        /// <param name="e">The exception.</param>
+        public static void DisplayErrorMessage(Exception e)
+        {
+            MessageBox.Show(e.ToString(), e.Message);
+        }
+    }
+}
