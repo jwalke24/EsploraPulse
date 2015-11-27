7db393dd1a092de94ad863f613ce06e6f78c2224
Jonathan Walker <jwalke24@my.westga.edu>
2015-11-27 14:49:46 -0500

    I implemented very basic EsploraPulseController and SerialReader classes. They can currently be instantiated, but don't actually contain any functionality.

---
+++ EsploraPulse/Controller/EsploraPulseController.cs
@@ -0,0 +1,23 @@
+﻿using EsploraPulse.Model;
+
+namespace EsploraPulse.Controller
+{
+    /// <summary>
+    /// This class is responsible for creating/managing PulseReading objects and
+    /// the SerialReader object.
+    /// </summary>
+    /// <author>Jonathan Walker</author>
+    /// <version>11/27/2015</version>
+    class EsploraPulseController
+    {
+        private SerialReader reader;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EsploraPulseController"/> class.
+        /// </summary>
+        public EsploraPulseController()
+        {
+            this.reader = new SerialReader();
+        }
+    }
+}
--- EsploraPulse/View/EsploraPulseForm.cs
+++ EsploraPulse/View/EsploraPulseForm.cs
@@ -1,20 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
+using EsploraPulse.Controller;
 
-namespace EsploraPulse
+namespace EsploraPulse.View
 {
+    /// <summary>
+    /// This class represents the code-behind for the EsploraPulse form,
+    /// which is responsible for displaying PulseReadings.
+    /// </summary>
+    /// <author>Jonathan Walker</author>
+    /// <version>11/27/2015</version>
     public partial class EsploraPulseForm : Form
     {
+        private EsploraPulseController controller;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EsploraPulseForm"/> class.
+        /// </summary>
         public EsploraPulseForm()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+
+            this.controller = new EsploraPulseController();
         }
     }
 }
